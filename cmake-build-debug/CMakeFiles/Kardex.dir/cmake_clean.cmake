653fe8d85eb33e0c8f2b68b640f1c5e63e6bf892
ADASTONRECON <giovanni.miranda@pucp.pe>
2019-09-12 20:52:13 -0500

    Nuevo Commit CLION

---
+++ cmake-build-debug/CMakeFiles/Kardex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kardex.dir/main.cpp.o"
+  "Kardex.pdb"
+  "Kardex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kardex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Kardex.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Kardex.
+# This may be replaced when dependencies are built.
